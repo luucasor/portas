9cec0f4fccbabe3184c22bec19c975c0d6a831e7
luucasor <lucas@jornadatecnologia.com>
2023-04-10 10:47:15 -0300

    Adicionando parte visual da seleção da porta com CSS

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,8 +3,6 @@ import Porta from "@/components/Porta"
 export default function Home() {
   return (
     <div style={{display: "flex"}}>
-      <Porta selecionada={true}/>
-      <Porta selecionada={false}/>
       <Porta selecionada/>
       <Porta />
     </div>
