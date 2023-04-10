5f48ba1a78056f3e9fe800620b23a7e39b001476
luucasor <lucas@jornadatecnologia.com>
2023-04-10 11:14:13 -0300

    Criando modelo para Porta.ts já integrado com TypeScript e interface

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -1,11 +1,18 @@
 import styles from "../styles/Porta.module.css"
-export default function Porta(props) {
-    const selecionada = props.selecionada ? styles.selecionada : '';
+import PortaModel from "../model/porta"
+
+interface PortaProps {
+    porta: PortaModel
+}
+
+export default function Porta(props: PortaProps) {
+    const { porta } = props
+    const selecionada = porta.selecionada ? styles.selecionada : '';
     return (
         <div className={styles.area}>
             <div className={`${styles.estrutura} ${selecionada}`}>
                 <div className={styles.porta}>
-                    <div className={styles.numero}>3</div>
+                    <div className={styles.numero}>{porta.numero}</div>
                     <div className={styles.macaneta}/>
                 </div>
             </div>
