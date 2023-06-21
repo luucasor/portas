a70eead0a56a1393fa123e597b0e6bba7756f04b
luucasor <lucas@jornadatecnologia.com>
2023-06-21 16:24:22 -0300

    adicionada validação de formulário

--- pages/jogo/[portas]/[temPresente].tsx
+++ pages/jogo/[portas]/[temPresente].tsx
@@ -7,7 +7,18 @@ import { useRouter } from "next/router"
 
 export default function jogo() {
     const router = useRouter()
+    const [valido, setValido] = useState(false)
     const [portas, setPortas] = useState([])
+
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+        
+        const qtdePortasValida = portas >= 3 && portas <= 100
+        const temPresenteValido = temPresente >= 1 && temPresente <= portas
+        
+        setValido(qtdePortasValida && temPresenteValido)
+    }, [portas])
 
     useEffect(() => {
         const portas = +router.query.portas
@@ -26,7 +37,10 @@ export default function jogo() {
     return (
         <div id={styles.jogo}>
             <div className={styles.portas}>
-                {renderizarPortas()}
+                {valido ?
+                    renderizarPortas() :
+                    <h1>Valores inválidos!</h1>
+                }
             </div>
             <div className={styles.botoes}>
                 <Link href="/">
